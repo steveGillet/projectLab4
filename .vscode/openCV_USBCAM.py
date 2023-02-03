614ea27c850b93ff2b5849fbc051a15d0873710b
Rishikesh <rishikesh2715@gmail.com>
2023-02-03 00:50:25 -0600

    Added Camera Control Variables

--- .vscode/openCV_USBCAM.py
+++ .vscode/openCV_USBCAM.py
@@ -1,11 +1,18 @@
 import cv2
 #print(cv2.__version__)
+
+width=800
+height=600
+flip=0
+
+camSet = 'v4l2src device=/dev/video1 ! video/x-raw,width='+str(width)+',height='+str(height)+',framerate=24/1 ! videoconvert ! appsink'
 cam=cv2.VideoCapture('/dev/video1')
 while True:
     _, frame = cam.read()
-    cv2.imshow('mycam',frame)
+    cv2.imshow('mycam',frame)\
+    #cv2.moveWindow('myCam',frame)
     if cv2.waitKey(1)==ord('e'):
         break
 
 cam.release()
-cv2.destroyAllWindows    
+cv2.destroyAllWindows() 
