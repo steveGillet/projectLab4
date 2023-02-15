c8728ea48f43b49af362293b6ccc1ac526ba1aa4
steveGillet <stepen.gillet@gmail.com>
2023-02-15 00:31:35 -0600

    integrating red square detection with jetson cam

--- boxDetect.py
+++ boxDetect.py
@@ -1,21 +1,25 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('redBoxWithOtherShapes.png', cv2.IMREAD_UNCHANGED)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
 lowerRed = np.array([-20, 100, 100])
 upperRed = np.array([13, 255, 255])
+# img = cv2.imread('redBoxWithOtherShapes.png', cv2.IMREAD_UNCHANGED)
 
-mask = cv2.inRange(hsv, lowerRed, upperRed)
-
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-imgContours = np.zeros(img.shape)
-cv2.drawContours(imgContours, contours, -1, (0,255,0), 3)
-cv2.imshow('contours',imgContours) 
-
+width=1280
+height=720
+flip=2
+camSet='nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1,format=NV12 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(width)+', height='+str(height)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam=cv2.VideoCapture(camSet)
 while True:
-  k = cv2.waitKey(5) & 0xFF
+  _, frame = cam.read()
+  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+  mask = cv2.inRange(hsv, lowerRed, upperRed)
+  contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  imgContours = np.zeros(frame.shape)
+  cv2.drawContours(imgContours, contours, -1, (0,255,0), 3)
+  cv2.imshow('contours',imgContours)
+  k = cv2.waitKey(5000)
   if k == 27:
     break
+cam.release()
+cv2.destroyAllWindows()
