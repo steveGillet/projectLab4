a78f3bee8df0acaee87d878eb6dfc97b53eb570f
steveGillet <89035893+steveGillet@users.noreply.github.com>
2023-02-18 19:51:10 -0600

    red door

--- detectDoor.py
+++ detectDoor.py
@@ -10,11 +10,23 @@ cap = cv2.VideoCapture(camSet)
 while True:
     _, frame = cap.read()
 
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Convert the frame to HSV color space
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Define the lower and upper bounds of the red color
+    lower_red = np.array([0, 50, 50])
+    upper_red = np.array([10, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    lower_red = np.array([170, 50, 50])
+    upper_red = np.array([180, 255, 255])
+    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Combine the two masks
+    mask = cv2.bitwise_or(mask1, mask2)
 
     # Apply a Gaussian blur to reduce noise
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    blur = cv2.GaussianBlur(mask, (5, 5), 0)
 
     # Threshold the image to make it binary
     _, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY)
@@ -22,17 +34,19 @@ while True:
     # Find contours in the binary image
     contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Look for door frame shape
+    # Look for red, square door frame shape
     for contour in contours:
         approx = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour, True), True)
         if len(approx) == 4:
             x,y,w,h = cv2.boundingRect(contour)
             aspect_ratio = float(w)/h
             if aspect_ratio > 0.8 and aspect_ratio < 1.2:
-                cv2.drawContours(frame, [contour], 0, (0, 255, 0), 3)
+                red_pixels = cv2.countNonZero(mask[y:y+h, x:x+w])
+                if red_pixels > 0.2 * w * h:
+                    cv2.drawContours(frame, [contour], 0, (0, 255, 0), 3)
 
     # Show the image
-    cv2.imshow("Door frame detection", frame)
+    cv2.imshow("Red, square door frame detection", frame)
 
     # Exit the loop if 'q' is pressed
     if cv2.waitKey(1) == ord('q'):
