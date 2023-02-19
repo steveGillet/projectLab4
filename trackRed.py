9a0d49727354c0b77e193cdb94bc26055fe34b18
steveGillet <stepen.gillet@gmail.com>
2023-02-18 18:53:14 -0600

    take pictures, improved color detect, get hsv

---
+++ trackRed.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+lowerRed = np.array([0, 170, 30])
+upperRed = np.array([10, 255, 90])
+
+width=1280
+height=720
+flip=0
+camSet='nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1,format=NV12 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(width)+', height='+str(height)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam=cv2.VideoCapture(camSet)
+
+while True:
+    ret,frame = cam.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lowerRed, upperRed)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if len(contours) > 0:
+        largestContour = max(contours, key=cv2.contourArea)
+        x,y,w,h = cv2.boundingRect(largestContour)
+
+        cv2.rectangle(frame,(x,y), (x + w, y + h), (0,255,0), 2)
+
+        centerX = x + w / 2
+        centerY = y + h / 2
+
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
