ad9bcfb5ec0c7ec7d554c40d6b5d5a4a73f267c4
steveGillet <stepen.gillet@gmail.com>
2023-02-19 20:13:32 -0600

    enables plugged into 3.3s

--- moveRobot.py
+++ moveRobot.py
@@ -3,31 +3,29 @@ import numpy as np
 import RPi.GPIO as GPIO
 import time
 
-in4 = 37
-in3 = 35
-in2 = 36
-in1 = 38
-enb = 33
-ena = 32
+in4 = 38
+in3 = 36
+in2 = 35
+in1 = 31
+# enb = 27
+# ena = 29
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(in3, GPIO.OUT)
 GPIO.setup(in4, GPIO.OUT)
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(ena, GPIO.OUT)
-GPIO.setup(enb, GPIO.OUT)
-pwm1 = GPIO.PWM(ena,60)
-pwm2 = GPIO.PWM(enb,60)
+# GPIO.setup(ena, GPIO.OUT)
+# GPIO.setup(enb, GPIO.OUT)
+
 
 def turnLeft():
-    pwm2.start(100)
     GPIO.output(in1, GPIO.HIGH)
-    GPIO.output(in3, GPIO.HIGH)
+    GPIO.output(in3, GPIO.LOW)
     GPIO.output(in2, GPIO.LOW)
-    GPIO.output(in4, GPIO.LOW)
-    pwm1.start(100)
-    pwm1.ChangeDutyCycle(100)
+    GPIO.output(in4, GPIO.HIGH)
+    # GPIO.output(ena, GPIO.HIGH)
+    # GPIO.output(enb, GPIO.HIGH)
 
 def stopMoving():
     GPIO.output(in1, GPIO.LOW)
@@ -35,8 +33,10 @@ def stopMoving():
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in4, GPIO.LOW)
     # GPIO.output(ena, GPIO.LOW)
+    # GPIO.output(enb, GPIO.LOW)
+    # GPIO.output(ena, GPIO.LOW)
 
 turnLeft()
-time.sleep(60)
+time.sleep(1 )
 stopMoving()
 GPIO.cleanup()
