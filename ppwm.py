626661b1674f24f5c516aff4fac201c2d41b0c32
steveGillet <stephen.gillet@gmail.com>
2023-03-15 19:53:02 -0500

    pwm testing

--- ppwm.py
+++ ppwm.py
@@ -1,6 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
+output_pins = {
+    'JETSON_XAVIER': 18,
+    'JETSON_NANO': 33,
+    'JETSON_NX': 33,
+    'CLARA_AGX_XAVIER': 18,
+    'JETSON_TX2_NX': 32,
+    'JETSON_ORIN': 18,
+    'JETSON_ORIN_NX': 33
+}
+output_pin = output_pins.get(GPIO.model, None)
+if output_pin is None:
+    raise Exception('PWM not supported on this board')
 
 
 def main():
@@ -9,7 +21,7 @@ def main():
     GPIO.setmode(GPIO.BOARD)
     # set pin as an output pin with optional initial state of HIGH
     GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
-    p = GPIO.PWM(33, 60)
+    p = GPIO.PWM(33, 50)
     val = 25
     incr = 5
     p.start(val)
