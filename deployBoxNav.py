9bab149b4c5b9826a4eb6ce857f227dee75a5454
steveGillet <stephen.gillet@gmail.com>
2023-03-16 21:55:50 -0500

    raspi cam working

--- deployBoxNav.py
+++ deployBoxNav.py
@@ -1,31 +1,41 @@
 import tensorflow as tf
 from tensorflow import keras
 import cv2
+from picamera2 import Picamera2
+import time
+import numpy as np
 
 model = keras.models.load_model('cnn_model.h5')
 
-width=1280
-height=720
-flip=0 
-camSet='nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1,format=NV12 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(width)+', height='+str(height)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-cap = cv2.VideoCapture(camSet)
+cv2.startWindowThread()
 
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.start()
+
+# Loop over frames from the camera
 while True:
-    ret, frame = cap.read()
-    
-    # Preprocess the frame
-    frame = cv2.resize(frame, (64, 64))
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = frame / 255.0
-    frame = frame.reshape(1, 64, 64, 3)
-    
-    # Make a prediction
-    prediction = model.predict(frame)
-    label = ['left', 'right', 'forward'][prediction.argmax()]
-    
-    # Display the label on the frame
-    cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow('frame', frame)
-    
+    # Preprocess the image for the CNN model
+    frame = picam2.capture_array()
+    frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    print(frame.shape)
+    print(frame.dtype)
+    frame2 = cv2.resize(frame1, (64, 64))
+    frame2 = frame2/ 255.0
+    frame2 = np.reshape(frame2, (1, 64, 64, 3))
+
+    # Run the CNN model on the preprocessed image
+    predictions = model.predict(frame2)
+    label = ['left', 'right', 'forward'][predictions.argmax()]
+    print(label)
+
+    # Display the image and predicted label in a window
+    cv2.imshow("Image", frame1)
+    cv2.waitKey(1)
+
+    # Exit the loop if the 'q' key is pressed
     if cv2.waitKey(1) == ord('q'):
         break
+
+# Clean up the camera and OpenCV resources
+cv2.destroyAllWindows()
---
+++ rasppicamtest.py
@@ -0,0 +1,18 @@
+import time
+from picamera2 import Picamera2, Preview
+
+picam = Picamera2()
+
+config = picam.create_preview_configuration()
+picam.configure(config)
+
+picam.start_preview(Preview.QTGL)
+
+picam.start()
+
+while True:
+    time.sleep(2)
+    
+picam.capture_file("test-python.jpg")
+
+picam.close()
