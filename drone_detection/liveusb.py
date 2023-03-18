11d19179398e9b4c30acec692da23f5e9e1a4678
rishikesh2715 <rishikesh.rishikesh@ttu.edu>
2023-03-18 02:45:27 -0500

    added drone detection

---
+++ drone_detection/liveusb.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+model = YOLO("D:\downloads\\tello.pt")
+
+
+model.predict(source="0",show=True,conf=0.5)
+
+#results = model.track(source="0", show=True, tracker="customTrack.yaml") 
