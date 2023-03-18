3b14a4c64bd1d60c109255cf8f552d2b1daef3ed
steveGillet <stephen.gillet@gmail.com>
2023-03-19 07:13:57 +0800

    testing and taking picture on orange pi

--- drone_detection/liveusb.py
+++ drone_detection/liveusb.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
-model = YOLO("D:\downloads\\tello.pt")
+model = YOLO("tello.pt")
 
 
 model.predict(source="0",show=True,conf=0.5)
