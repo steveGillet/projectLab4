dd0709e495cc6c6fb42e9fcaeb0e6685602d6bc5
steveGillet <stephen.gillet@gmail.com>
2023-03-24 23:44:43 +0000

    orange pi pwm and gpios working

--- gpio.py
+++ gpio.py
@@ -3,53 +3,116 @@ import time
 from wiringpi import GPIO
 import subprocess
 
-# wiringpi.wiringPiSetup()
-# wiringpi.pinMode(8, GPIO.OUTPUT)
+ena = 2
+enb = 5
+in1 = 7
+in2 = 8
+in3 = 11
+in4 = 12
 
-# wiringpi.digitalWrite(8, GPIO.HIGH)
-# time.sleep(2)
-# wiringpi.digitalWrite(8, GPIO.LOW)
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(in1, GPIO.OUTPUT)
+wiringpi.pinMode(in2, GPIO.OUTPUT)
+wiringpi.pinMode(in3, GPIO.OUTPUT)
+wiringpi.pinMode(in4, GPIO.OUTPUT)
 
 def runCommand(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     return stdout.decode().strip(), stderr.decode().strip()
 
-def fillCommandString(first, chip, channel="", command=""):
+def fillCommandString(first, chip, command, channel=""):
     if channel=="":
-        return f"echo {first} > /sys/class/pwm/{chip}/export"
+        return f"echo {first} > /sys/class/pwm/{chip}/{command}"
     else:
         return f"echo {first} > /sys/class/pwm/{chip}/pwm{channel}/{command}"
 
-# pwm_chip = "pwmchip1"
-# channel = 0
+def initializePWM():
+    stdout, stderr = runCommand(fillCommandString(0, 'pwmchip4', 'export'))
+    if stderr:
+        print(f"Error: {stderr}")
 
-stdout, stderr = runCommand(fillCommandString(0, 'pwmchip4'))
-if stderr:
-    print(f"Error: {stderr}")
+    stdout, stderr = runCommand(fillCommandString(60, 'pwmchip4', 'period', 0))
+    if stderr:
+        print(f"Error: {stderr}")
 
-# period_ns = 20000000  # 20ms period
-# duty_cycle_ns = 1500000  # 1.5ms duty cycle (adjust as needed)
+    stdout, stderr = runCommand(fillCommandString(0, 'pwmchip5', 'export'))
+    if stderr:
+        print(f"Error: {stderr}")
 
+    stdout, stderr = runCommand(fillCommandString(60, 'pwmchip5', 'period', 0))
+    if stderr:
+        print(f"Error: {stderr}")
 
-stdout, stderr = runCommand(fillCommandString(60, 'pwmchip4', 0, 'period'))
-if stderr:
-    print(f"Error: {stderr}")
+def pulseWidth(chip, duty):
+    if chip == 2:
+        chip = 'pwmchip5'
+    if chip == 5:
+        chip = 'pwmchip4'
+    stdout, stderr = runCommand(fillCommandString(duty, chip, 'duty_cycle', 0))
+    if stderr:
+        print(f"Error: {stderr}")
 
-stdout, stderr = runCommand(fillCommandString(20, 'pwmchip4', 0, 'duty_cycle'))
-if stderr:
-    print(f"Error: {stderr}")
+    if duty != 60:
+        stdout, stderr = runCommand(fillCommandString(1, chip, 'enable', 0))
+        if stderr:
+            print(f"Error: {stderr}")
+    else:
+        stdout, stderr = runCommand(fillCommandString(0, chip, 'enable', 0))
+        if stderr:
+            print(f"Error: {stderr}")
 
-stdout, stderr = runCommand(fillCommandString(1, 'pwmchip4', 0, 'enable'))
-if stderr:
-    print(f"Error: {stderr}")
+def closePWM():
+    stdout, stderr = runCommand(fillCommandString(0, 'pwmchip4', 'enable', 0))
+    if stderr:
+        print(f"Error: {stderr}")
+    stdout, stderr = runCommand(fillCommandString(0, 'pwmchip4', 'unexport'))
+    if stderr:
+        print(f"Error: {stderr}")
+    stdout, stderr = runCommand(fillCommandString(0, 'pwmchip5', 'enable', 0))
+    if stderr:
+        print(f"Error: {stderr}")
+    stdout, stderr = runCommand(fillCommandString(0, 'pwmchip5', 'unexport'))
+    if stderr:
+        print(f"Error: {stderr}")
 
-time.sleep(2)
+def stop():
+    wiringpi.digitalWrite(in1, GPIO.LOW)
+    wiringpi.digitalWrite(in2, GPIO.LOW)
+    wiringpi.digitalWrite(in3, GPIO.LOW)
+    wiringpi.digitalWrite(in4, GPIO.LOW)
+    pulseWidth(ena, 60)
+    pulseWidth(enb, 60)
 
-stdout, stderr = runCommand(fillCommandString(10, 'pwmchip4', 0, 'duty_cycle'))
-if stderr:
-    print(f"Error: {stderr}")
+def turnLeft():
+    wiringpi.digitalWrite(in1, GPIO.HIGH)
+    wiringpi.digitalWrite(in2, GPIO.LOW)
+    wiringpi.digitalWrite(in3, GPIO.LOW)
+    wiringpi.digitalWrite(in4, GPIO.HIGH)
+    pulseWidth(ena, 20)
+    pulseWidth(enb, 20)
 
-stdout, stderr = runCommand(fillCommandString(0, 'pwmchip4', 0, 'enable'))
-if stderr:
-    print(f"Error: {stderr}")
+def turnRight():
+    wiringpi.digitalWrite(in1, GPIO.LOW)
+    wiringpi.digitalWrite(in2, GPIO.HIGH)
+    wiringpi.digitalWrite(in3, GPIO.HIGH)
+    wiringpi.digitalWrite(in4, GPIO.LOW)
+    pulseWidth(ena, 20)   
+    pulseWidth(enb, 20)
+
+def forward():
+    wiringpi.digitalWrite(in1, GPIO.HIGH)
+    wiringpi.digitalWrite(in2, GPIO.LOW)
+    wiringpi.digitalWrite(in3, GPIO.HIGH)
+    wiringpi.digitalWrite(in4, GPIO.LOW)
+    pulseWidth(ena, 20)    
+    pulseWidth(enb, 20)
+
+initializePWM()
+turnLeft()
+time.sleep(1)
+turnRight()
+time.sleep(1)
+stop()
+
+closePWM()
